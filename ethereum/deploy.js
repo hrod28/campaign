0cdfff7e06454c4a165c4e6edfd9c6007c49880b
Heidi Rodriguez <hrod2645@gmail.com>
2018-05-04 21:37:48 -0600

    end to end test

---
+++ ethereum/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+const provider = new HDWalletProvider(
+  'inherit pioneer absent north future critic tongue ritual since desk word symbol',
+  'https://rinkeby.infura.io/u6ur2TOxBK9Fcgdp0DOi'
+
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: '0x' + compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
